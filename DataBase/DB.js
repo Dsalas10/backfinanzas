0c6e113d3fd93077ffa424a896fc139f85daeffc
daniel salas <dasalass15@gmail.com>
2025-10-16 15:28:04 -0400

    ajustes db

--- DataBase/DB.js
+++ DataBase/DB.js
@@ -1,22 +1,34 @@
-const mongoose = require("mongoose");
-// Conexion a base de datos
+// const mongoose = require("mongoose");
+// // Conexion a base de datos
+// // const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ugqzyrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const dbName = process.env.DBNAME;
 // const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ugqzyrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const dbName = process.env.DBNAME;
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ugqzyrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-mongoose.connect(uri, { dbName})
-    .then(() => console.log("✅ Base de Datos MongoDB conectada correctamente"))
-    .catch((e) => {
-        console.error("❌ Error al conectar la BD:", e.message);
-        process.exit(1);
+// mongoose.connect(uri, { dbName})
+//     .then(() => console.log("✅ Base de Datos MongoDB conectada correctamente"))
+//     .catch((e) => {
+//         console.error("❌ Error al conectar la BD:", e.message);
+//         process.exit(1);
+//     });
+
+// mongoose.connection.on('disconnected', () => {
+//     console.warn('⚠️  Se perdió la conexión con MongoDB');
+// });
+
+// mongoose.connection.on('error', (err) => {
+//     console.error('❌ Error en la conexión de MongoDB:', err);
+// });
+
+ const mongoose = require('mongoose');
+    mongoose.connect(process.env.URL)
+      .then(() => console.log('✅ Base de Datos MongoDB conectada correctamente'))
+      .catch(err => console.error('❌ Error al conectar la BD:', err));
+      mongoose.connection.on('disconnected', () => {
+        console.warn('⚠️  Se perdió la conexión con MongoDB');
     });
 
-mongoose.connection.on('disconnected', () => {
-    console.warn('⚠️  Se perdió la conexión con MongoDB');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error('❌ Error en la conexión de MongoDB:', err);
-});
+    mongoose.connection.on('error', (err) => {
+        console.error('❌ Error en la conexión de MongoDB:', err);
+    });
 
 module.exports = mongoose;
